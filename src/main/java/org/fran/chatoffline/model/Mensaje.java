fa7f8f7751dd874b55a5bb09fa8db51ea5d9e4db
FranciscoSolerAguado <franciscojsoleraguado@gmail.com>
2025-10-11 12:38:34 +0200

    Clase Mensaje Atributos, getter y setter, constructor etc...

--- src/main/java/org/fran/chatoffline/model/Mensaje.java
+++ src/main/java/org/fran/chatoffline/model/Mensaje.java
@@ -1,4 +1,104 @@
 package org.fran.chatoffline.model;
 
+import java.time.LocalDateTime;
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "mensaje")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Mensaje {
+
+    @XmlAttribute
+    private String idMensaje;
+
+    @XmlElement
+    private String remitente;
+
+    @XmlElement
+    private String destinatario;
+
+    @XmlElement
+    private String contenido;
+
+    @XmlElement
+    private LocalDateTime fechaEnvio;
+
+    @XmlElement
+    private Adjunto adjunto;  // Puede ser null si no hay adjunto
+
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String remitente, String destinatario, String contenido) {
+        this.idMensaje = java.util.UUID.randomUUID().toString();
+        this.remitente = remitente;
+        this.destinatario = destinatario;
+        this.contenido = contenido;
+        this.fechaEnvio = LocalDateTime.now();
+    }
+
+    public Mensaje(String remitente, String destinatario, String contenido, Adjunto adjunto) {
+        this(remitente, destinatario, contenido);
+        this.adjunto = adjunto;
+    }
+
+
+    public String getIdMensaje() {
+        return idMensaje;
+    }
+
+    public void setIdMensaje(String idMensaje) {
+        this.idMensaje = idMensaje;
+    }
+
+    public String getRemitente() {
+        return remitente;
+    }
+
+    public void setRemitente(String remitente) {
+        this.remitente = remitente;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public LocalDateTime getFechaEnvio() {
+        return fechaEnvio;
+    }
+
+    public void setFechaEnvio(LocalDateTime fechaEnvio) {
+        this.fechaEnvio = fechaEnvio;
+    }
+
+    public Adjunto getAdjunto() {
+        return adjunto;
+    }
+
+    public void setAdjunto(Adjunto adjunto) {
+        this.adjunto = adjunto;
+    }
+
+
+    public boolean tieneAdjunto() {
+        return adjunto != null;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + fechaEnvio + "] " + remitente + ": " + contenido +
+                (tieneAdjunto() ? " (Adjunto: " + adjunto.getNombreArchivo() + ")" : "");
+    }
 }
