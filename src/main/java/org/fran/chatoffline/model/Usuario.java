fa86ab6bd6fc97aa8f5b9f923f05adfde841e74f
FranciscoSolerAguado <franciscojsoleraguado@gmail.com>
2025-10-11 12:35:50 +0200

    Clase Usuario Atributos, getter y setter, constructor etc...

--- src/main/java/org/fran/chatoffline/model/Usuario.java
+++ src/main/java/org/fran/chatoffline/model/Usuario.java
@@ -1,5 +1,37 @@
 package org.fran.chatoffline.model;
 
 public class Usuario {
+    private String nombre;
+    private String telefono;
+    private String estado;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Usuario(String nombre, String telefono, String estado) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.estado = estado;
+    }
 }
