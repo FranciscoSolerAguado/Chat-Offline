72268b3892916e391f5bd42a4138d71e97099c02
FranciscoSolerAguado <franciscojsoleraguado@gmail.com>
2025-10-12 11:49:01 +0200

    Pantalla principal

--- src/main/java/org/fran/chatoffline/controller/MainController.java
+++ src/main/java/org/fran/chatoffline/controller/MainController.java
@@ -1,8 +1,85 @@
 package org.fran.chatoffline.controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
 import java.util.logging.Logger;
 
 public class MainController {
     private static final Logger LOGGER = Logger.getLogger(ConversacionController.class.getName());
+
+    @FXML
+    private HBox topBar;
+
+    @FXML
+    private Label chatLabel;
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @FXML
+    private void initialize() {
+        topBar.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        topBar.setOnMouseDragged(event -> {
+            Stage stage = (Stage) topBar.getScene().getWindow();
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+    }
+
+    /**
+     * Método que maneja el evento de minimizar la ventana
+     */
+    @FXML
+    private void handleMinimize() {
+        Stage stage = (Stage) topBar.getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    /**
+     * Método que maneja el evento de maximizar o restaurar la ventana.
+     */
+    private boolean isMaximized = false;
+
+    @FXML
+    private void handleToggleMaximize() {
+        Stage stage = (Stage) topBar.getScene().getWindow();
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+
+        if (isMaximized) {
+            // Restaurar tamaño y centrar
+            stage.setWidth(1200);
+            stage.setHeight(800);
+            stage.setX(screenBounds.getMinX() + (screenBounds.getWidth() - 1000) / 2);
+            stage.setY(screenBounds.getMinY() + (screenBounds.getHeight() - 700) / 2);
+            isMaximized = false;
+        } else {
+            // Maximizar manualmente
+            stage.setX(screenBounds.getMinX());
+            stage.setY(screenBounds.getMinY());
+            stage.setWidth(screenBounds.getWidth());
+            stage.setHeight(screenBounds.getHeight());
+            isMaximized = true;
+        }
+    }
+
+    /**
+     * Método que maneja el evento de cerrar la aplicación.
+     */
+    @FXML
+    private void handleClose() {
+        Platform.exit();
+    }
+
 
 }
